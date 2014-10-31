fd47c03404db5346c50a8731b9caf40578c66c0f
XadillaX <admin@xcoder.in>
2014-10-31 16:05:18 +0800

    upload dir

--- pfh.js
+++ pfh.js
@@ -10,7 +10,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
-app.use(express.bodyParser());
+app.use(express.bodyParser({ uploadDir: path.join(__dirname, "./uploadTemp") }));
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
